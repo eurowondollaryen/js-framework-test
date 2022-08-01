f9e27f2eae441c2a12fa1261af128929f2516910
eurowondollaryen <sehoakasayho@naver.com>
2022-08-02 00:24:27 +0900

    aggrid done

--- vue-form/src/components/TabOne.vue
+++ vue-form/src/components/TabOne.vue
@@ -1,13 +1,34 @@
 <template>
   <div>this is tab1</div>
+  <ag-grid-vue
+    style="width: 500px; height: 200px"
+    class="ag-theme-alpine"
+    :columnDefs="columnDefs"
+    :rowData="rowData">
+  </ag-grid-vue>
 </template>
 
 <script>
 import { AgGridVue } from "ag-grid-vue3";
 
 export default {
+  name: "TabOne",
   components: {
     AgGridVue
+  },
+  data: function() {
+    return {
+      columnDefs: [
+        { headerName: "Make", field: "make" },
+        { headerName: "Model", field: "model" },
+        { headerName: "Price", field: "price" }
+      ],
+      rowData: [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxster", price: 72000 },
+      ]
+    };
   },
   methods: {
     showGrid: function() {
