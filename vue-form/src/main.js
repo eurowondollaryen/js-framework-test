2701c5425dfd71f8e518b00bfec7288c3f9faa92
Seho Kim <sehoakasayho@gmail.com>
2022-09-30 20:58:08 +0900

    bootstrap test for vue-form

--- vue-form/src/main.js
+++ vue-form/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import "ag-grid-community/styles//ag-grid.css";
 import "ag-grid-community/styles//ag-theme-alpine.css";
 
