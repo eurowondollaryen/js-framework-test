adc099e890037283f280919d4125af63ff66b190
eurowondollaryen <sehoakasayho@naver.com>
2022-10-09 22:47:29 +0900

    drag and drop

---
+++ drag-and-drop/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <file-upload msg="Welcome to Your Vue.js App"></file-upload>
+</template>
+
+<script>
+import FileUpload from './components/FileUpload.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FileUpload
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ drag-and-drop/src/components/FileUpload.vue
@@ -0,0 +1,39 @@
+<template>
+  <h3>{{msg}}</h3>
+  <div class="area-file-list"></div>
+  <div class="area-file-drop" @drop="fileDrop" @dragover="allowDrop">
+    여기에 파일을 드롭하세요
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileUpload',
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      fileList: []
+    }
+  },
+  methods: {
+    fileDrop: function(e) {
+      e.preventDefault();
+      const droppedFiles = e.dataTransfer.files;
+      console.log("[fileDrop]", droppedFiles);
+    },
+    allowDrop: function(e) {
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style>
+.area-file-drop {
+  border: dashed;
+  width: 100%;
+  height: 200px;
+}
+</style>
