12c2a9633546cc57f1a95b283fe4b42e641a6e69
eurowondollaryen <sehoakasayho@naver.com>
2022-10-10 23:57:50 +0900

    file drag and drop

--- drag-and-drop/src/components/FileUpload.vue
+++ drag-and-drop/src/components/FileUpload.vue
@@ -1,6 +1,10 @@
 <template>
   <h3>{{msg}}</h3>
-  <div class="area-file-list"></div>
+  <div class="area-file-list">
+    <ul>
+      <li v-for="file in fileList" :key="file.src">{{file.name}}</li>
+    </ul>
+  </div>
   <div class="area-file-drop" @drop="fileDrop" @dragover="allowDrop">
     여기에 파일을 드롭하세요
   </div>
@@ -21,10 +25,38 @@ export default {
     fileDrop: function(e) {
       e.preventDefault();
       const droppedFiles = e.dataTransfer.files;
-      console.log("[fileDrop]", droppedFiles);
+      this.addFiles(droppedFiles);
     },
     allowDrop: function(e) {
       e.preventDefault();
+    },
+    addFiles: async function(fileList) {
+      //파일 추가 후 첨부파일 수가 제한될 경우, return false
+      if(fileList.length + this.fileList.length > this.fileNumberLimit) {
+        alert("파일 개수는 " + this.fileNumberLimit + "개를 넘을 수 없습니다.");
+        return false;
+      }
+      for(let i = 0; i < fileList.length; ++i) {
+        const src = await this.readFiles(fileList[i]);
+        //console.log(i);
+        //console.log(fileList[i].src);
+        //console.log(src);
+        fileList[i].src = src;
+        this.fileList.push(fileList[i]);
+      }
+      console.log(this.fileList);
+    },
+    readFiles: async function(fileList) {
+      return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+          resolve(e.target.result);
+        }
+        reader.readAsDataURL(fileList);
+      });
+    },
+    handleRemove: function(index) {
+      this.fileList.splice(index, 1);
     }
   }
 }
